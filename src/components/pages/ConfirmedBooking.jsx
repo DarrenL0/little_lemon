f29b6c9a8caff9ce8fb462773b331dfb640a7337
DarrenL0 <darren.lieu2000@gmail.com>
2025-03-30 19:30:10 +1100

    Added css to Confirmation Page

--- src/components/pages/ConfirmedBooking.jsx
+++ src/components/pages/ConfirmedBooking.jsx
@@ -1,12 +1,25 @@
 import { Link } from "react-router-dom";
 
+
 const ConfirmedBooking = () => {
   return (
-    <div className="confirmed-booking" aria-live="assertive">
-      <h1>Booking Confirmed!</h1>
-      <p>Your reservation has been successfully booked.</p>
-      <Link to="/" aria-label="Return to the homepage">Return to Home</Link>
-    </div>
+    <section className="bookingConfirmed">
+      <div className="Confirmedcard">
+          <h2>Booking Confirmed!</h2>
+          <p className="text-gray-600 mt-2">Your reservation has been successfully booked.</p>
+          
+          <div className="buttonContainer">
+            <Link
+              to="/"
+              className="button-primary confirmedReturn"
+              aria-label="Return to the homepage"
+            >
+              Return to Home
+            </Link>
+          </div>
+        
+      </div>
+    </section>
   );
 };
 
